42caaebded19875ea31e39accc99c1492c739724
FaithSlade <faithwslade@gmail.com>
2021-05-27 20:10:48 -0700

    committing all the work

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello World");
         //enhancing hello
         //work 1
+        //work 2
     }
 }
---
+++ src/test/java/ResetPractice.java
@@ -0,0 +1,9 @@
+public class ResetPractice {
+    public static void main(String[] args) {
+        //work 4
+
+        //work 5 is done after resetting hard way
+
+        //committing all together
+    }
+}
